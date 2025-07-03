eb683f357e7f1cb9216f519752bdb424e80be1f1
Risqiddin Rustamov <risqiddinrustamov@Risqiddins-MacBook-Pro-2.local>
2025-07-03 12:29:05 +0500

    init project

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import reactCompiler from "eslint-plugin-react-compiler"
+import tseslint from "typescript-eslint"
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler
+    },
+
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-shadow": "off",
+      "no-shadow": "off",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      // React Compiler rules
+      "react-compiler/react-compiler": "error"
+    }
+  }
+)
