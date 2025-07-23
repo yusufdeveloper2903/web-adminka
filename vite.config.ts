2ae309a9a51f65d5ff266d2480ddcdc2bb588e18
Risqiddin Rustamov <risqiddinrustamov@Risqiddins-MacBook-Pro-2.local>
2025-07-23 14:23:29 +0500

    fix: allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig(({ mode }) => {
     server: serverOptions,
     preview: {
       ...serverOptions,
-      allowedHosts: ["dev-admin.glmiler.com", "admin.glmiler.com"]
+      allowedHosts: ["dev.glmiler.com", "glmiler.com"]
     },
 
     plugins: [
