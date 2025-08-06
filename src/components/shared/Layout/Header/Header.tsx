1919966ba06a08cce0ba8282d8eb086ac453e7d2
Risqiddin Rustamov <risqiddinrustamov@Risqiddins-MacBook-Pro-2.local>
2025-08-06 19:47:07 +0500

    fix: trip header issues

--- src/components/shared/Layout/Header/Header.tsx
+++ src/components/shared/Layout/Header/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
       </div>
 
       {/* Right Side */}
-      <section className="flex items-center gap-4 max-2xl:overflow-x-auto">
+      <section className="flex items-center gap-4 max-2xl:overflow-x-auto custom-scrollbar">
         {metadata && <div className="text-muted-foreground text-sm whitespace-pre">{metadata}</div>}
         {actions.map(({ id, label, onClick, variant, disabled, icon }) => (
           <Button key={id} onClick={onClick} variant={variant || "default"} size="sm" disabled={disabled}>
